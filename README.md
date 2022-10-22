dadae390bc94a62605bb6c4585f9da7ed7c37fa5
gustavohpereira <gustavohpa2003@gmail.com>
2022-10-21 22:00:07 -0300

    Project Commit

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
-# Credit-Card-Creation
-A project made with the rocketseat class named "explorer lab" 
+<h1 align="center"> Explorer Lab #01 </h1>
 
-Consist of a dynamic page that changes the information of the card with the information provided.The card senses if is a Visa or Mastercard card, changing the color and logo in the process.
-The purpose of this page is JavaScript DOM manipulation practice, and use of Imaskjs for the forms
+<p align="center">
+Evento exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="rocketpay" src=".github/project.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript e JSON
+- [Node e NPM](https://nodejs.org/)
+- [Vite](https://vitejs.dev/)
+- [iMask](https://imask.js.org)
+
+## 💻 Projeto
+
+O Rocketpay é um componente que simula o formulário de preenchimento de cartão de crédito, onde é possível adicionar máscara aos inputs e atualizar elementos HTML via DOM.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/gpqavL469k0pPUGOmAQEM9/Explorer-Lab-%2301/duplicate). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ by Rocketseat :wave: [Participe da nossa comunidade!](https://discord.gg/rocketseat)
---
+++ src/main.js
@@ -0,0 +1,150 @@
+import "./css/index.css"
+import IMask from "imask"
+
+
+// Alteração da cor do cartão
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
+
+
+const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
+// alteração logo
+    const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+
+
+function setCardType(type){
+    const colors = {
+        'Visa': [ "#436D99", "#2D57F2"] , 
+        'Mastercard' : ["#DF6F29","#C69347"],
+        'Default': ["black","gray"],
+
+    }
+    
+    ccBgColor01.setAttribute('fill', colors[type][0]),
+
+    ccBgColor02.setAttribute('fill', colors[type][1])
+    ccLogo.setAttribute('src' , `cc-${type}.svg`)
+
+
+}
+globalThis.setCardType = setCardType
+
+
+const inputCVC = document.querySelector('#security-code')
+const securityCodePattern = {
+    mask:'0000'
+}
+const securityCodeMasked = IMask(inputCVC,securityCodePattern)
+
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+    mask:'MM{/}YY',
+    blocks:{
+        MM: {
+            mask: IMask.MaskedRange,
+            from: 1 ,
+            to: 12
+
+        },
+        YY:{
+            mask: IMask.MaskedRange,
+            from: 22,
+            to: 32
+        }
+    },
+}
+const expirationDateMasked = IMask(expirationDate,expirationDatePattern)
+
+const CardNumber = document.querySelector('#card-number')
+const cardNumberPattern = {
+    mask: [
+        {
+        mask: "0000 0000 0000 0000",
+        regex:/^4\d{0,15}/,
+        cardtype: 'Visa'
+        },
+        {
+            mask: "0000 0000 0000 0000", 
+            regex:/(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,2}/,
+            cardtype: 'Mastercard'
+        },
+        {
+            mask: "0000 0000 0000 0000", 
+            cardtype: 'Default'
+        }
+    ],
+ dispatch: function(appended, dynamicMasked){
+     var number = (dynamicMasked.value + appended).replace(/\D/g,'');
+     const foundMask = dynamicMasked.compiledMasks.find(function(item){
+        return number.match(item.regex)
+     })
+     
+
+     return foundMask
+ } 
+
+
+
+}
+const CardNumberMasked = IMask(CardNumber,cardNumberPattern)
+
+
+
+
+const addButton = document.querySelector('#botão_adicionar')
+addButton.addEventListener('click',() => {
+    console.log('clicou')
+
+})
+document.querySelector('form').addEventListener('submit',(event) =>{
+    event.preventDefault()
+})
+
+const CardHolder = document.querySelector('#card-holder')
+CardHolder.addEventListener('input', () =>{
+    const ccHolder = document.querySelector('.cc-holder .value')
+    ccHolder.innerText = CardHolder.value.length === 0 ? "Fulano da silva" : CardHolder.value
+
+})
+
+securityCodeMasked.on("accept",() =>{
+
+  updateSecurityCode(securityCodeMasked.value)
+    
+
+})
+
+
+function updateSecurityCode(code){
+    const ccSecurity = document.querySelector('.cc-security .value')
+    ccSecurity.innerText = code.length === 0 ? "123" : code
+    
+}
+
+CardNumberMasked.on('accept',() => {
+    const cardType = CardNumberMasked.masked.currentMask.cardtype
+    setCardType(cardType)
+    updateCardNumber(CardNumberMasked.value)
+
+})
+function updateCardNumber(number){
+
+    const ccNumber = document.querySelector('.cc-number')
+    ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+   updateExpirationDate(expirationDateMasked.value)
+    
+})
+function updateExpirationDate(expiration){
+     const ccExpiration = document.querySelector('.cc-expiration .value')
+     ccExpiration.innerText = expiration.length === 0 ? '02/32' : expiration
+}
+
+
+
+
+ 
+
